99d036fe915082303c12a73636c02389be8cc28c
Kevin Later <laterkevin@icloud.com>
2022-07-12 11:20:56 +0200

    Aufgabe bearbeitet!

--- app/src/main/java/de/syntax_institut/filmestreamingservice/ui/HomeFragment.kt
+++ app/src/main/java/de/syntax_institut/filmestreamingservice/ui/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import de.syntax_institut.filmestreamingservice.R
+import de.syntax_institut.filmestreamingservice.adapter.ItemAdapter
 import de.syntax_institut.filmestreamingservice.data.Datasource
 import de.syntax_institut.filmestreamingservice.databinding.FragmentHomeBinding
 
@@ -18,6 +21,7 @@ class HomeFragment : Fragment() {
     ): View? {
 
        // TODO: lade das Layout in die BindingVariable
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
         return binding.root
     }
@@ -27,6 +31,8 @@ class HomeFragment : Fragment() {
         val movieTitles = Datasource(requireContext()).loadMovies()
 
         //TODO: lade den ItemAdapter in die RecyclerView
+        val recyclerView = binding.recyclerViewHome
+        recyclerView.adapter = ItemAdapter(requireContext(), movieTitles)
+        recyclerView.setHasFixedSize(true)
     }
-
 }
